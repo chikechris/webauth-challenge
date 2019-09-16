e11c9331b9dbd5e7dddc9337397c7767166ec994
chikechris <cchike2017@gmail.com>
2019-09-16 16:00:23 -0400

    added userHelper and userRoute

---
+++ routes/usersRoute.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bcrypt = require('bcryptjs')
+
+const userDb = require('./usersModel.js')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Users Route' })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express')
-
+const usersRoute = require('./routes/usersRoute.js')
 
 const server = express()
 
 server.use(express.json())
 
-
+server.use('/api/users', usersRoute)
 
 server.use('/', (req, res) => {
   res.send(`<h2>Api Home</h2>`)
 })
 
 module.exports = server
+
