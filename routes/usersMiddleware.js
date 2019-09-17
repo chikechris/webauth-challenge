e1de7e46f16daffa21d57020218f202a0cc873fe
chikechris <cchike2017@gmail.com>
2019-09-17 14:46:11 -0400

    added cookies validation

--- routes/usersMiddleware.js
+++ routes/usersMiddleware.js
@@ -1,38 +1,26 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 const userDb = require('./usersHelper.js')
 
-function validateBody (req, res, next) {
-  const { username, password } = req.body
+function validateBody(req, res, next) {
+  const { username, password } = req.body;
   if (username && password) {
-    next()
+    next();
   } else {
-    res
-      .status(401)
-      .json({ message: 'Provide username or password' })
+    res.status(401).json({ message: "Provide username or password" })
   }
 }
 
-function restricted (req, res, next) {
-  const { username, password } = req.headers
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
 
-  if (username && password) {
-    userDb
-      .findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next()
-        } else {
-          res.status(401).json({ message: 'Access Denied' })
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error)
-      })
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ message: 'Access Denied' })
   }
 }
 
 module.exports = {
   validateBody,
   restricted
-}
+};
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const usersRoute = require('./routes/usersRoute.js')
+const session = require('express-session');
 
 const server = express()
 
 server.use(express.json())
+
+server.use(
+  session({
+    name: 'chocoDip',
+    secret: 'keep it a secret',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: false,
+      httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+
 
 server.use('/api', usersRoute)
 
